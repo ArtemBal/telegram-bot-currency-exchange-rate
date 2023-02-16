a650a3b004aa685d2c61d990b00942e15e6943a8
Artem Balan <a.s.balan96@gmail.com>
2023-02-16 22:49:34 +0200

    Create project,bot configurations

---
+++ src/main/java/artem/balan/telegrambotcurrencyexchangerate/TelegramBotCurrencyExchangeRateApplication.java
@@ -0,0 +1,13 @@
+package artem.balan.telegrambotcurrencyexchangerate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotCurrencyExchangeRateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotCurrencyExchangeRateApplication.class, args);
+	}
+
+}
---
+++ src/main/java/artem/balan/telegrambotcurrencyexchangerate/config/BotConfig.java
@@ -0,0 +1,16 @@
+package artem.balan.telegrambotcurrencyexchangerate.config;
+
+import lombok.Data;
+import lombok.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@Data
+@PropertySource("application.properties")
+public class BotConfig {
+    @Value("${bot.name}")
+    String botName;
+    @Value("${bot.token}")
+    String token;
+}
