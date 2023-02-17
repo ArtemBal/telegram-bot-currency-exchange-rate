ad8f78b2f3541e95c6c4b7463d75ed192269feae
Artem Balan <a.s.balan96@gmail.com>
2023-02-17 17:18:12 +0200

    Fix fucking jackson and capital letter properties error

--- src/main/java/artem/balan/telegrambotcurrencyexchangerate/model/CurrencyModel.java
+++ src/main/java/artem/balan/telegrambotcurrencyexchangerate/model/CurrencyModel.java
@@ -1,15 +1,15 @@
 package artem.balan.telegrambotcurrencyexchangerate.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
-import java.util.Date;
-
 @Data
+@JsonFormat(with = JsonFormat.Feature.ACCEPT_CASE_INSENSITIVE_PROPERTIES)
 public class CurrencyModel {
-    Date StartDate;
-    int TimeSign;
-    int CurrencyCode;
-    String CurrencyCodeL;
-    int Units;
-    double Amount;
+    private String startDate;
+    private String timeSign;
+    private String currencyCode;
+    private String currencyCodeL;
+    private int units;
+    private double amount;
 }
--- src/main/java/artem/balan/telegrambotcurrencyexchangerate/service/CurrencyService.java
+++ src/main/java/artem/balan/telegrambotcurrencyexchangerate/service/CurrencyService.java
@@ -2,30 +2,17 @@ package artem.balan.telegrambotcurrencyexchangerate.service;
 
 import artem.balan.telegrambotcurrencyexchangerate.model.CurrencyModel;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
-import java.lang.reflect.Type;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 
 public class CurrencyService {
     public static String getCurrencyRate() throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         ArrayList<CurrencyModel> list = objectMapper.readValue(new URL("https://bank.gov.ua/NBU_Exchange/exchange?json"),
-                new TypeReference<ArrayList<CurrencyModel>>() {
-            @Override
-            public Type getType() {
-                return super.getType();
-            }
-        });
+                new TypeReference<>() {});
         return list.toString();
-    }
-
-    public static void main(String[] args) throws IOException {
-        System.out.println(getCurrencyRate());
     }
 }
